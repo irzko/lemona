1a46c418d96ac81de06aff870b3b91c968b6d8ba
Kha <53520595+irzko@users.noreply.github.com>
2024-04-16 03:39:50 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <Script
           id="Absence-banner"
-          strategy="lazyOnload"
+          strategy="beforeInteractive"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4568231404553117"
           crossOrigin="anonymous"
