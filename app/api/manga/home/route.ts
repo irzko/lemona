407bc6ae6e048548b260016166523b3cac5c1eb2
Kha <53520595+irzko@users.noreply.github.com>
2024-04-18 03:38:48 +0700

    Update route.ts

--- app/api/manga/home/route.ts
+++ app/api/manga/home/route.ts
@@ -3,14 +3,14 @@ import prisma from "@/lib/prisma";
 export async function GET(req: Request) {
   const recentlyUpdated = await prisma.manga.findMany({
     orderBy: {
-      updatedAt: { sort: "desc"},
+      updatedAt: "desc",
     },
     take: 12,
   });
 
   const latest = await prisma.manga.findMany({
     orderBy: {
-      createdAt: { sort: "desc"},
+      createdAt: "desc",
     },
     take: 12,
   });
