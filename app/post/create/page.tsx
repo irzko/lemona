f70dea0cd05b9aff6de5d8098fbd8a8000648e47
Kha <53520595+irzko@users.noreply.github.com>
2024-11-18 19:23:08 +0000

    Update

---
+++ app/post/create/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { Suspense } from "react";
+import Editor from "@/components/editor"
+import {type MDXEditorMethods} from "@mdxeditor/editor"
+import {useRef} from "react"
+
+
+
+const markdown = `
+Hello **world**!
+`
+export default function Page() {
+  const ref = useRef<MDXEditorMethods>(null)
+  return (
+    <div style={{ border: "1px solid black" }}>
+      <Suspense fallback={<div>Đang tải editor...</div>}>
+        <Editor markdown={markdown} />
+      </Suspense>
+    </div>
+  );
+}
---
+++ components/editor/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+import dynamic from "next/dynamic";
+import { forwardRef } from "react";
+import { type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
+
+const MDXEditor = dynamic(() => import("./InitializedMDXEditor"), {
+  ssr: false,
+});
+
+const Editor = forwardRef<MDXEditorMethods, MDXEditorProps>((props, ref) => (
+  <MDXEditor {...props} editorRef={ref} />
+));
+
+export default Editor;
