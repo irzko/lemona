01e50931ab13e4178b6066c2b5bf89ddf5163766
Kha <53520595+irzko@users.noreply.github.com>
2024-11-19 17:02:38 +0700

    Update route.ts

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request) => {
+export const GET = async () => {
   const posts = await prisma.post.findMany();
   return NextResponse.json(posts);
 }
