67b8c5c4ceb9b68441a3b069712e4e2dc69724e9
Kha <53520595+irzko@users.noreply.github.com>
2024-11-20 11:54:51 +0000

    Update

--- app/actions.ts
+++ app/actions.ts
@@ -6,14 +6,12 @@ import { redirect } from "next/navigation";
 export async function createPost(formData:FormData) {
   const title = formData.get("title") as string;
   const content = formData.get("content") as string;
-  console.log("8282828")
   await prisma.post.create({
     data: {
       title: title,
       content: content,
     },
-  });
-  console.log(`Created post with title: ${title}`);
-  revalidateTag("post");
+  })!
+  revalidateTag("posts");
   redirect("/");
 }
