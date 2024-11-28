b46cd656438e574b9d46ccab764e09a820cac020
Kha <53520595+irzko@users.noreply.github.com>
2024-11-28 18:01:27 +0700

    Update index.tsx

--- components/lexical/plugins/ToolbarPlugin/index.tsx
+++ components/lexical/plugins/ToolbarPlugin/index.tsx
@@ -810,7 +810,7 @@ export default function ToolbarPlugin({
   // const canViewerSeeInsertCodeButton = !toolbarState.isImageCaption;
 
   return (
-    <div className="sticky z-43 top-16 px-3 py-2 border-b flex items-center flex-wrap gap-2 rounded-t-lg bg-gray-50">
+    <div className="sticky z-30 top-16 px-3 py-2 border-b flex items-center flex-wrap gap-2 rounded-t-lg bg-gray-50">
       <Button
         disabled={!toolbarState.canUndo || !isEditable}
         onClick={() => {
