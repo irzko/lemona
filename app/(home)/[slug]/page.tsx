5ba667e74e9cb62490a8d36afc6b926cea64c1ef
Kha <53520595+irzko@users.noreply.github.com>
2024-12-02 08:18:32 +0000

    .

--- app/(home)/[slug]/page.tsx
+++ app/(home)/[slug]/page.tsx
@@ -9,7 +9,8 @@ import remarkIns from "remark-ins";
 import Image from "next/image";
 import Link from "next/link";
 import rehypeHighlight from "rehype-highlight";
-// import 'react-lowlight/common'
+import "react-lowlight/all";
+
 //import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 //import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
