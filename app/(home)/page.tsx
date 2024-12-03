361793edf6ed41b63ab96ef3e09838de6be7473e
Kha <53520595+irzko@users.noreply.github.com>
2024-12-03 04:59:42 +0000

    .

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -29,20 +29,27 @@ export default async function Page() {
     <main className="flex justify-center">
       <div className="max-w-screen-lg w-full p-4">
         <h3 className="mb-4">Bài viết mới</h3>
-        <ul className="grid grid-cols-2 space-y-0 list-none sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-4">
+        <ul className="grid grid-cols space-y-0 list-none sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
           {allPosts.slice(0, 4).map((post) => (
             <li className="overflow-hidden bg-white" key={post.id}>
-              <Link
-                className="text-gray-800"
-                href={`/${post.slug}`}
-              >
-                <div className="relative w-full aspect-video">
-                  <Image
-                    src={post.featuredImageURL || "/no-image.jpg"}
-                    alt={post.title}
-                    fill
-                    className="object-cover rounded-lg"
-                  />
+              <Link className="text-gray-800" href={`/${post.slug}`}>
+                <div>
+                  <div className="relative z-10 w-full aspect-video">
+                    <Image
+                      src={post.featuredImageURL || "/no-image.jpg"}
+                      alt={post.title}
+                      fill
+                      className="object-cover rounded-xl"
+                    />
+                  </div>
+                  <div className="absolute z-0 inset-0 w-full aspect-video">
+                    <Image
+                      className="object-cover rounded-xl filter blur-lg scale-105 saturate-150 opacity-30 translate-y-1"
+                      src={post.featuredImageURL || "/no-image.jpg"}
+                      alt={post.title}
+                      fill
+                    />
+                  </div>
                 </div>
 
                 <h6 className="pt-2 font-semibold text-base line-clamp-3">
@@ -55,10 +62,7 @@ export default async function Page() {
         <ul className="grid grid-cols-1 mt-4 space-y-0 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
           {allPosts.slice(4).map((post) => (
             <li className="overflow-hidden bg-white" key={post.id}>
-              <Link
-                className="text-gray-800 flex gap-4"
-                href={`/${post.slug}`}
-              >
+              <Link className="text-gray-800 flex gap-4" href={`/${post.slug}`}>
                 <div className="w-2/5">
                   <div className="relative w-full aspect-video">
                     <Image
