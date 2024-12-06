0d095126997c342465dbc7caec4eb64235d5aa7d
Kha <53520595+irzko@users.noreply.github.com>
2024-12-06 11:02:27 +0000

    Update

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,8 +1,8 @@
 import prisma from "@/lib/prisma";
-import { getCategoryTree } from '@prisma/client/sql'
+import { getCategoryTree } from "@prisma/client/sql";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const categoryTree = await prisma.$queryRawTyped(getCategoryTree())
+  const categoryTree = await prisma.$queryRawTyped(getCategoryTree());
   return NextResponse.json(categoryTree);
 };
