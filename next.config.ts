9d2d01df3a3b4bf30ff8a8045afcacbd8fca7379
Kha <53520595+irzko@users.noreply.github.com>
2024-12-08 03:58:06 +0000

    Remove dynamicIO from Next.js configuration to streamline experimental features

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    dynamicIO: true,
     serverActions: {
       allowedOrigins: [
         "friendly-waddle-j9g4vw94762x6v-3000.app.github.dev",
