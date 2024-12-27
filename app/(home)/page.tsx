53cac4ce5132618ac3087756b968aa4fccffd273
Kha <53520595+irzko@users.noreply.github.com>
2024-12-27 06:14:36 +0000

    Add NextUI Card components for post display; implement loading spinner

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import Link from "next/link";
 import Image from "next/image";
 import slugify from "slugify";
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
 
 const getPosts = unstable_cache(
   async () => {
@@ -32,66 +33,68 @@ export default async function Page() {
         <h3 className="text-2xl">Bài viết mới</h3>
         <ul className="grid grid-cols divide-y sm:divide-y-0 sm:grid-cols-2 md:grid-cols-4 gap-4">
           {allPosts.slice(0, 4).map((post) => (
-            <li className="bg-white pt-4" key={post.id}>
-              <Link
-                href={`/${slugify(post.title, {
-                  replacement: "-",
-                  remove: undefined,
-                  lower: true,
-                  strict: true,
-                  locale: "vi",
-                  trim: true,
-                })}-${post.id}.html`}
-              >
-                <div className="relative w-full aspect-video">
-                  <Image
-                    src={post.featuredImageURL || "/no-image.jpg"}
-                    alt={post.title}
-                    fill
-                    unoptimized
-                    className="object-cover rounded-lg"
-                  />
-                </div>
-                <div className="">
-                  <h3 className="font-bold mt-4 uppercase text-lg line-clamp-3">
-                    {post.title || "(No title)"}
-                  </h3>
-                </div>
-              </Link>
-            </li>
+            <Card
+              as={Link}
+              href={`/${slugify(post.title, {
+                replacement: "-",
+                remove: undefined,
+                lower: true,
+                strict: true,
+                locale: "vi",
+                trim: true,
+              })}-${post.id}.html`}
+              className="bg-white pt-4"
+              isPressable
+              key={post.id}
+            >
+              <CardBody className="relative w-full aspect-video">
+                <Image
+                  src={post.featuredImageURL || "/no-image.jpg"}
+                  alt={post.title}
+                  fill
+                  unoptimized
+                  className="object-cover rounded-lg"
+                />
+              </CardBody>
+              <CardFooter>
+                <h3 className="font-bold mt-4 uppercase text-lg line-clamp-3">
+                  {post.title || "(No title)"}
+                </h3>
+              </CardFooter>
+            </Card>
           ))}
         </ul>
         <ul className="grid grid-cols-1 mt-4 space-y-0 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
           {allPosts.slice(4).map((post) => (
-            <li className="overflow-hidden bg-white" key={post.id}>
-              <Link
-                className="text-gray-800 flex gap-4"
-                href={`/${slugify(post.title, {
-                  replacement: "-",
-                  remove: undefined,
-                  lower: true,
-                  strict: true,
-                  locale: "vi",
-                  trim: true,
-                })}-${post.id}.html`}
-              >
-                <div className="w-2/5">
-                  <div className="relative w-full aspect-video">
-                    <Image
-                      src={post.title || "/no-image.jpg"}
-                      alt={post.title}
-                      unoptimized
-                      fill
-                      className="object-cover rounded-lg"
-                    />
-                  </div>
-                </div>
-
-                <h6 className="font-semibold w-3/5 text-base line-clamp-3">
+            <Card
+              as={Link}
+              href={`/${slugify(post.title, {
+                replacement: "-",
+                remove: undefined,
+                lower: true,
+                strict: true,
+                locale: "vi",
+                trim: true,
+              })}-${post.id}.html`}
+              className="bg-white"
+              isPressable
+              key={post.id}
+            >
+              <CardBody className="relative w-full aspect-video">
+                <Image
+                  src={post.featuredImageURL || "/no-image.jpg"}
+                  alt={post.title}
+                  fill
+                  unoptimized
+                  className="object-cover rounded-lg"
+                />
+              </CardBody>
+              <CardFooter>
+                <h3 className="font-bold mt-4 uppercase text-lg line-clamp-3">
                   {post.title || "(No title)"}
-                </h6>
-              </Link>
-            </li>
+                </h3>
+              </CardFooter>
+            </Card>
           ))}
         </ul>
       </div>
